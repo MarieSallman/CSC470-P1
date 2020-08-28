0e0104df39aa6eee7216b52888a35fbd4e867d7c
Marie Sallman <Marie.Sallman@trojans.dsu.edu>
2020-08-27 23:16:20 -0500

    Final rename changes to form and text.

--- P1 Code/P1 Code/HelloWorldForm.Designer.cs
+++ P1 Code/P1 Code/HelloWorldForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace P1_Code
 {
-    partial class Form1
+    partial class HelloWorldForm
     {
         /// <summary>
         /// Required designer variable.
@@ -44,23 +44,29 @@
             // 
             // Exit
             // 
-            this.Exit.Location = new System.Drawing.Point(690, 395);
+            this.Exit.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Exit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Exit.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(619, 335);
             this.Exit.Name = "Exit";
             this.Exit.Size = new System.Drawing.Size(109, 57);
             this.Exit.TabIndex = 1;
             this.Exit.Text = "Exit";
-            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.UseVisualStyleBackColor = false;
             this.Exit.Click += new System.EventHandler(this.Exit_Click);
             // 
-            // Form1
+            // HelloWorldForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.Exit;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.Exit);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "HelloWorldForm";
+            this.Text = "CSC 470 - P1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
